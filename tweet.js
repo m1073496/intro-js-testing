e298fb729502f1367ce9222d80a83deeba668724
Jessica Justice <jessica@jessicas-MacBook-Pro.local>
2020-12-08 14:45:55 -0700

    Complete tweet tests

--- tweet.js
+++ tweet.js
@@ -1,5 +1,26 @@
 class Tweet {
+  constructor(handle, text) {
+    this.sender = handle;
+    this.content = text;
+    this.likes = 0;
+    this.retweets = 0;
+    this.replies = [];
+    this.replyCount = 0;
+  };
 
-}
+  like() {
+    this.likes = this.likes + 1;
+  };
+
+  retweet() {
+    this.retweets = this.retweets + 1;
+  };
+
+  reply(replyTxt) {
+    this.replies.push(replyTxt);
+    this.replyCount = this.replyCount + 1;
+  };
+
+};
 
 module.exports = Tweet;
